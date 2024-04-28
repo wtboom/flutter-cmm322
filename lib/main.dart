885052a29def568f02423d917d8c45e0eec0e708
Sutthiphong <azballkung@Sutthiphongs-MacBook-Air.local>
2024-04-28 20:38:04 +0700

    binding and delete web platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'dart:html';
-
 import 'package:binny_application/firebase_options.dart';
 import 'package:binny_application/pages/homepage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   //Initialize Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
